6ed89dc2f445f2cbb42e224c7a7ffe80aaa1a198
sandarma190180088 <sandarma190180088@mhs.unimal.ac.id>
2023-03-03 02:04:00 +0700

    update file var-size.c

--- sources/var-size.c
+++ sources/var-size.c
@@ -8,6 +8,12 @@ int main(void){
   print_type_size(char);
   print_type_size(short);
   print_type_size(int);
-  print_type_size(long long);
+  print_type_size(float);
+  print_type_size(double);
+  print_type_size(long);
+  
+  char name[] = "Sandy";
+  print_type_size(name);
+  printf("nama anda adalah : %s\n",name);
   return 0;
 }
